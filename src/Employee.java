b1b49749b938ac8376e1f32dbc61000c25775faf
Андрей <andrews.ru83@gmail.com>
2022-05-26 01:03:21 +0300

    Получен список сотрудников

---
+++ src/Employee.java
@@ -0,0 +1,45 @@
+public class Employee {
+
+    public String name ;
+    public int department ;
+    public int salary ;
+
+    public int id ;
+
+    private static int counter = 0 ;
+
+    public Employee(String name, int department, int salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        id = counter ++ ;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+    @Override
+    public String toString(){
+        return name + ", отдел: " + department + ", з/п: " + salary;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
-    public static void main(String[] args) {
 
+    static Employee [] fullName = new Employee[10] ;
+        public static void main(String[] args) {
+            fullName[0] = new Employee("Иванов Иван Иванович", 1, 50_000) ;
+            fullName[1] = new Employee("Петров Петр Петрович", 2, 55_000) ;
+            fullName[2] = new Employee("Сидоров Сидр Сидорович", 3, 75_500) ;
+            fullName[3] = new Employee("Ефимова Алла Николаевна", 4, 81_500) ;
+            fullName[4] = new Employee("Москвин Анатолий Борисович", 5, 92_000) ;
+            fullName[5] = new Employee("Фомина Наталья Аркадьевна", 1, 32_000) ;
+            fullName[6] = new Employee("Лебедева Оксана Федоровна", 2, 28_200) ;
+            fullName[7] = new Employee("Миронова Оксана Владимировна", 3, 68_400) ;
+            fullName[8] = new Employee("Васин Василий Антонович", 4, 27_900) ;
+            fullName[9] = new Employee("Симонов Семен Васильевич", 5, 88_000) ;
 
-    }
+            print() ;
+            }
+
+        public static void print() {
+            for (Employee employee : fullName) {
+                System.out.println(employee);
+            }
+        }
+
 }
